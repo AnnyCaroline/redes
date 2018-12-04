152886e9793c9c29ecd390ba5cf25482c3686e4e
Anny Caroline <annycarolinegnr@gmail.com>
2018-12-04 11:05:17 -0200

    ...

--- redes-adhoc.cc
+++ redes-adhoc.cc
@@ -1,34 +1,62 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
- /*
-  * Copyright (c) 2015, IMDEA Networks Institute
-  *
-  * This program is free software; you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License version 2 as
-  * published by the Free Software Foundation;
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program; if not, write to the Free Software
-  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-  *
-  * Author: Hany Assasa <hany.assasa@gmail.com>
- .*
-  * This is a simple example to test TCP over 802.11n (with MPDU aggregation enabled).
-  *
-  * Network topology:
-  *
-  *    0-Sink
-  *  1
-  *
-  * In this example, an HT station sends TCP packets to the access point.
-  * We report the total throughput received during a window of 100ms.
-  * The user can specify the application data rate and choose the variant
-  * of TCP i.e. congestion control algorithm to use.
-  */
+/*
+* Copyright (c) 2015, IMDEA Networks Institute
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation;
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+* Author: Anny Caroline C. Chagas <annycarolinegnr@gmail.com>
+*
+*
+* Network topology:
+*
+*   ------5 Nós-------
+*   ------------------
+*
+*   Árvore 1
+*   0(192.92.236.1 - sink)
+*             |
+*      1(192.92.236.2)
+*             |
+*      2(192.92.236.3)
+*             |  
+*      3(192.92.236.4)
+*             |  
+*      4(192.92.236.5)
+*
+*
+*
+*   Árvore 2
+*                    0(192.92.236.1 - sink)
+*                     /                 \
+*             1(192.92.236.2)      2(192.92.236.3)
+*              /          \
+*   3(192.92.236.4)  4(192.92.236.5)
+*
+*
+*
+*  Árvore 3
+*                           0(192.92.236.1 - sink)
+*                             |  |            |  |
+*           __________________|  |            |  |_____________
+*          /                     |            |                \
+*   1(192.92.236.2)     2(192.92.236.3)   3(192.92.236.4)  4(192.92.236.5)
+*
+*
+* Nessa simulação, todos os filhos (todos os nós menos o 0), enviam dados para o nó raiz (0).
+* Mediamos a vazão recebida em um intervalo de X.
+* O usuário pode especificar o número de nós, a configuração da árvore e o data rate.
+*/
  
 #include "ns3/command-line.h"
 #include "ns3/config.h"
@@ -46,55 +74,53 @@
 #include "ns3/tcp-westwood.h"
 #include "ns3/internet-stack-helper.h"
 #include "ns3/ipv4-address-helper.h"
-
 #include "ns3/ipv4-global-routing-helper.h"
 #include "ns3/ipv4-static-routing-helper.h"
 #include "ns3/ipv4-list-routing-helper.h"
  
- NS_LOG_COMPONENT_DEFINE ("wifi-tcp");
+NS_LOG_COMPONENT_DEFINE ("wifi-tree");
+ 
+using namespace ns3;
+ 
+Ptr<PacketSink> sink;     // ponteiro para o nó sink, nó raiz
+uint64_t lastTotalRx = 0; // O valor para o último total de bytes recebidos
+std::ofstream myfile;
 
+void CalculateThroughput(){
+    //Simulator::Now obtém o tempo virtula do simulador
+    Time now = Simulator::Now ();
+
+    // Converte os pacotes recebidos para MBits
+    double cur = (sink->GetTotalRx() - lastTotalRx) * 8.0 / 1e6;
+    
+    // Imprime a vazão atual
+    std::cout << now.GetSeconds() << "s: \t" << cur << " Mbit/s" << std::endl;
+    myfile << now.GetSeconds() << "s: \t" << cur << " Mbit/s" << std::endl;
+    
+    lastTotalRx = sink->GetTotalRx();
+    
+    //Executará a rotina novamente depois de 1s
+    Simulator::Schedule (MilliSeconds(1000), &CalculateThroughput);
+}
  
- using namespace ns3;
- 
- Ptr<PacketSink> sink;                         /* Pointer to the packet sink application */
- uint64_t lastTotalRx = 0;                     /* The value of the last total received bytes */
- std::ofstream myfile;
- 
- void CalculateThroughput ()
- {
-   Time now = Simulator::Now ();                                         /* Return the simulator's virtual time. */
-   double cur = (sink->GetTotalRx() - lastTotalRx) * 8.0 / 1e6;     /* Convert Application RX Packets to MBits. */
-   std::cout << now.GetSeconds() << "s: \t" << cur << " Mbit/s" << std::endl;
-   myfile << now.GetSeconds() << "s: \t" << cur << " Mbit/s" << std::endl;
-   lastTotalRx = sink->GetTotalRx();
-   Simulator::Schedule (MilliSeconds(1000), &CalculateThroughput);
- }
- 
- int
- main (int argc, char *argv[]){
+int main (int argc, char *argv[]){
     uint32_t payloadSize = 1472;                /* Transport layer payload size in bytes. */
-    std::string protocol   =  "n";
     std::string tcpVariant = "TcpWestwoodPlus"; /* TCP variant type. */
-    std::string phyRate = "HtMcs7";             /* Physical layer bitrate. */
+    std::string phyRate = "OfdmRate54Mbps";     /* Physical layer bitrate. */
     double simulationTime = 10;                 /* Simulation time in seconds. */
     bool pcapTracing = false;                   /* PCAP Tracing is enabled or not. */
     int round = 1;
     int arvore = 1;
-    
- 
-   /* Command line argument parser setup. */
-   CommandLine cmd;
-   cmd.AddValue ("payloadSize", "Payload size in bytes", payloadSize);
-   cmd.AddValue ("arvore", "Arvore utilizada para o teste", arvore);
-   cmd.AddValue ("protocol", "802.11n or 802.11ac (n/ac)", protocol);
-   cmd.AddValue ("round", "Round", round);
-   cmd.AddValue ("tcpVariant", "Transport protocol to use: TcpNewReno, "
-                 "TcpHybla, TcpHighSpeed, TcpHtcp, TcpVegas, TcpScalable, TcpVeno, "
-                 "TcpBic, TcpYeah, TcpIllinois, TcpWestwood, TcpWestwoodPlus, TcpLedbat ", tcpVariant);
-   cmd.AddValue ("phyRate", "Physical layer bitrate", phyRate);
-   cmd.AddValue ("simulationTime", "Simulation time in seconds", simulationTime);
-   cmd.AddValue ("pcap", "Enable/disable PCAP Tracing", pcapTracing);
-   cmd.Parse (argc, argv);
+
+    /* Command line argument parser setup. */
+    CommandLine cmd;
+    cmd.AddValue ("payloadSize", "Payload size in bytes", payloadSize);
+    cmd.AddValue ("arvore", "Arvore utilizada para o teste", arvore);
+    cmd.AddValue ("round", "Round", round);
+    cmd.AddValue ("phyRate", "Physical layer bitrate", phyRate);
+    cmd.AddValue ("simulationTime", "Simulation time in seconds", simulationTime);
+    cmd.AddValue ("pcap", "Enable/disable PCAP Tracing", pcapTracing);
+    cmd.Parse (argc, argv);
 
     // Define um arquivo de saída
     std::string filename="Resultado_"+std::to_string(arvore)+"_"+phyRate+"_"+std::to_string(simulationTime)+"_"+std::to_string(round);
@@ -120,13 +146,7 @@
 
     WifiMacHelper wifiMac;
     WifiHelper wifiHelper;
-    if (protocol.compare("n") == 0){
-        std::cout << "n" << std::endl;
-        wifiHelper.SetStandard(WIFI_PHY_STANDARD_80211n_5GHZ);
-    }else{
-        std::cout << "ac" << std::endl;
-        wifiHelper.SetStandard(WIFI_PHY_STANDARD_80211ac); //IEEE 802.11ac operates in 5 GHz                
-    }
+    wifiHelper.SetStandard(WIFI_PHY_STANDARD_80211n_5GHZ);
  
     /* Set up Legacy Channel */
     YansWifiChannelHelper wifiChannel;
@@ -136,10 +156,8 @@
     /* Setup Physical Layer */
     YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default ();
     wifiPhy.SetChannel (wifiChannel.Create ());
-    wifiPhy.SetErrorRateModel ("ns3::YansErrorRateModel");
-    wifiHelper.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
-                                       "DataMode", StringValue (phyRate),
-                                       "ControlMode", StringValue ("HtMcs"));
+    wifiPhy.SetErrorRateModel("ns3::YansErrorRateModel");
+    wifiHelper.SetRemoteStationManager("ns3::ConstantRateWifiManager");
 
 	//Cria os nós: nodes contém [AP, STA]
 	NodeContainer nodes;
@@ -174,13 +192,19 @@
         positionAlloc->Add (Vector (0.0, 2.0, 0.0));
         positionAlloc->Add (Vector (0.0, 1.0, 0.0));
         positionAlloc->Add (Vector (0.0, 0.0, 0.0));  
+    }else if (arvore == 3){
+        positionAlloc->Add (Vector (0.0, 4.0, 0.0));
+        positionAlloc->Add (Vector (0.0, 3.0, 0.0));
+        positionAlloc->Add (Vector (0.0, 2.0, 0.0));
+        positionAlloc->Add (Vector (0.0, 1.0, 0.0));
+        positionAlloc->Add (Vector (0.0, 0.0, 0.0));  
     }
 
     mobility.SetPositionAllocator (positionAlloc);
     mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
     mobility.Install(nodes);
  
-    /* Internet stack */
+    // Internet stack
     InternetStackHelper stack;
     stack.Install(nodes);
 
@@ -188,9 +212,7 @@
     address.SetBase ("192.168.1.0", "255.255.255.0");
     Ipv4InterfaceContainer interfaces = address.Assign(devices);
 
-    /* Populate routing table */
-    //Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-    
+    // Popular a tabela de roteamento
     Ipv4StaticRoutingHelper ipv4RoutingHelper;
     Ptr<Ipv4StaticRouting> staticRouting0 = ipv4RoutingHelper.GetStaticRouting (nodes.Get(0)->GetObject<Ipv4>());
     Ptr<Ipv4StaticRouting> staticRouting1 = ipv4RoutingHelper.GetStaticRouting (nodes.Get(1)->GetObject<Ipv4>());
@@ -201,31 +223,32 @@
 
     if (arvore == 1){
         // 1 -> 0
-        staticRouting1->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.1"), 1);
-
-        // 2 -> 0
-        staticRouting2->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.1"), 1);
-
-        // 3 -> 0
-        staticRouting3->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.2"), 1);
-
-        // 4 -> 0   
-        staticRouting4->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.2"), 1);
-    }else if (arvore == 2){
-        // 0 -> 1
-        //staticRouting0->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.1"), 1);
-
-        // 1 -> 0
-        staticRouting1->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.1"), 1); //destination, nextHop, interface
-
+        staticRouting1->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.1"), 1); 
         // 2 -> 0
         staticRouting2->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.2"), 1);
-
         // 3 -> 0
         staticRouting3->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.3"), 1);
-
         // 4 -> 0
         staticRouting4->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.4"), 1);
+        //destination, nextHop, interface        
+    }else if (arvore == 2){
+        // 1 -> 0
+        staticRouting1->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.1"), 1);
+        // 2 -> 0
+        staticRouting2->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.1"), 1);
+        // 3 -> 0
+        staticRouting3->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.2"), 1);
+        // 4 -> 0   
+        staticRouting4->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.2"), 1);
+    }else if (arvore==3){
+        // 1 -> 0
+        staticRouting1->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.1"), 1);
+        // 2 -> 0
+        staticRouting2->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.1"), 1);
+        // 3 -> 0
+        staticRouting3->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.1"), 1);
+        // 4 -> 0   
+        staticRouting4->AddHostRouteTo(Ipv4Address ("192.168.1.1"), Ipv4Address ("192.168.1.1"), 1);        
     }
 
     /* Sink no nó raiz - 0 */
